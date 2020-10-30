d4aa860e269222ececa85c624abad8cad608aa3d
Atmosferrum <73058391+Atmosferrum@users.noreply.github.com>
2020-10-30 11:52:52 +0300

    Fisrt push
    Fisrt push

---
+++ Lesson 8.5 (Movement & Rotation)/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] runners;
+    [SerializeField] GameObject stick;
+    [SerializeField] float passDistance = 0.5f;
+    [SerializeField] bool forward = true;
+    [SerializeField] float speed = 5f;
+
+    float distance;
+    int i;
+
+    GameObject currentRunner;
+    GameObject nextRunner;
+
+    private void Start()
+    {
+        currentRunner = runners[0];
+
+        stick.transform.parent = currentRunner.transform;
+
+        if (forward)
+            i = 1;
+        else
+            i = 4;
+
+        nextRunner = runners[i];
+    }
+
+    private void Update()
+    {
+        currentRunner.transform.position = Vector3.MoveTowards(currentRunner.transform.position, nextRunner.transform.position, Time.deltaTime * speed);
+        currentRunner.transform.LookAt(nextRunner.transform);
+
+        distance = Vector3.Distance(currentRunner.transform.position, nextRunner.transform.position);
+
+        if(distance <= passDistance)
+        {
+            if (forward)
+            {
+                if (i == 4)
+                    i = 0;
+                else
+                    ++i;
+
+                Tag(i);
+            }
+            else
+            {
+                if (i == 0)
+                    i = 4;
+                else
+                    --i;
+
+                Tag(i);
+            }
+        }   
+    }
+
+    void Tag(int i)
+    {
+        currentRunner = nextRunner;
+        nextRunner = runners[i];
+        stick.transform.parent = currentRunner.transform;
+        stick.transform.localPosition = new Vector3(0, 1.5f, 0);
+    }
+}
---
+++ Lesson 8.5 (Movement & Rotation)/Assets/Scripts/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] bool canGo = true;
+    [SerializeField] float speed = 10;
+    [SerializeField] Transform[] checkpoints;
+    [SerializeField] Transform currentCheckpoint;
+
+    int i = 0;
+    
+    void Update()
+    {
+        
+        transform.Rotate(0, 0, 2);
+
+        if (canGo)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, checkpoint(), speed * Time.deltaTime);
+
+                            
+        }
+    }
+
+    Vector3 checkpoint()
+    {
+        
+
+        if (transform.position == currentCheckpoint.transform.position)
+        {
+            
+            if (i == 3)
+                i = 0;
+            else
+                ++i;
+
+            currentCheckpoint = checkpoints[i];
+            transform.LookAt(currentCheckpoint);
+            return checkpoints[i].transform.position;
+        }
+            
+
+        return currentCheckpoint.transform.position;
+    }
+
+
+}
